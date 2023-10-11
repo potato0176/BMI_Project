43e8465ff111049109cac56c7db39ee6f05ae5db
potato0176 <83850817+potato0176@users.noreply.github.com>
2023-10-11 21:58:30 +0800

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -1,1 +1,32 @@
-sdasda
+var readline = require("readline-sync"); 
+while(true){
+    var h=readline.question("Plz enter ur height in cm:");
+    if (isNaN(h)||h<0){
+        console.log("Eror!");
+        console.log("Plz reenter ur height!");
+    }else{
+        break;
+    }
+}
+while(true){
+    var w=readline.question("Plz enter ur weight in kg:");
+    if (isNaN(w)||w<0){
+        console.log("Eror!");
+        console.log("Plz reenter ur weight!");
+    }else{
+        break;
+    }  
+}
+BMI=w/(Math.pow((h/100),2));
+BMI = BMI.toFixed(2);
+console.log("ur height is %d cm",h);
+console.log("ur weight is %f kg",w);
+console.log("ur BMI is %f",BMI);
+
+if(BMI<18.5){
+    console.log("u r too thin.")
+}else if(18.5<=BMI<24){
+    console.log("u r normal.")
+}else{
+    console.log("u r too fat bruh.")
+}
