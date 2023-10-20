023730b047df6c999b6214fb9d8419cd08f77f42
potato0176 <chenfansoon0716@gmail.com>
2023-10-20 14:56:14 +0800

    asd

--- guess4d.js
+++ guess4d.js
@@ -1,42 +1,44 @@
 var readline = require("readline-sync");
-var ans=[];
-var counterB=0;
+var ans=[0,1,2,3,4,5,6,7,8,9];
+ans.sort(function(a, b){return Math.random()-0.5});
+answer=[];
 for(var x=0;x<4;x++){
-    var rand = Math.floor(Math.random()*10);
-    ans[x]=rand;
+    ans[x]=answer[x];
 }
-console.log(ans);
 console.log("U have ten chance to guess.");
 for(var i=0; i<10;i++){
     var counterA=0;
+    var counterB=0;
     do {
         var guess = readline.question("Please guess 4 digits?");
-        counterB++;
-        if (!isNaN(guess) && guess.length == 4)//repeat digit?
+        if (!isNaN(guess) && guess.length == 4)
             break;
         else {
             console.log("4 digits please!")
             continue;
         }
     }while (true);
-    console.log(counterB);
-    for(var a=0;a<4;a++){
-        if(guess[a]==ans[a]){
-            counterA++;
+    for (let ans_idx = 0; ans_idx < 4; ans_idx++) {
+       
+        for (let guess_idx = 0; guess_idx < 4; guess_idx++) {
+            if(guess[guess_idx] == answer[ans_idx]){
+                if(guess_idx==ans_idx){
+                    counterA++;
+                }
+                else{
+                    counterB++;
+                }
+            }
         }
+        console.log(counterA);
+        console.log(counterB);
     }
-    console.log(counterA);
+    console.log("Your guess:" + counterA+"A"+counterB+"B")
+
      if(counterA==4){
          console.log("You win the game!");
          break;
-    }
+     }
 }
-if(counterB<10){
-    console.log("U got %d chance less",(10-counterB));
-}else{
-    console.log("U lose all chances!Keep going!")
-}
-if(counterA<4){
-    console.log("You lose!");
-}
+
 
