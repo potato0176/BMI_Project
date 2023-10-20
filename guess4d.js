ed11daa46b82b7ef0be64b07a7cf7c35d78c8185
potato0176 <83850817+potato0176@users.noreply.github.com>
2023-10-20 10:58:24 +0800

    Add files via upload

---
+++ guess4d.js
@@ -0,0 +1,33 @@
+var readline = require("readline-sync");
+var ans=[];
+for(var x=0;x<4;x++){
+    var rand = Math.floor(Math.random()*10);
+    ans[x]=rand;
+}
+console.log(ans);
+do {
+    var guess = readline.question("Please guess 4 digits: ");
+
+    if (!isNaN(guess) && guess.length == 4)//repeat digit?
+        break;
+    else {
+        console.log("4 digits please!")
+        continue;
+    }
+} while (true);
+console.log(guess);
+var counterA=0;
+console.log("U have ten chance to guess.");
+for(var a=0;a<10;a++){
+for(var x=0;x<4;x++){
+    if(guess[x]==ans[x])
+    counterA++;
+}
+console.log(counterA);
+if(counterA==4){
+console.log("You win the game!");
+break;}
+else{
+console.log("You lose!");}
+}
+
